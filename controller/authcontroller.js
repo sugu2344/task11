e81376f6f971f0c65728feacfe92df3850643c74
sugu2344 <suganesh234@gmail.com>
2025-01-04 15:12:53 +0530

    controller basic setup done

---
+++ controller/authcontroller.js
@@ -0,0 +1,20 @@
+const auth = require("../models/users");
+const authController = {
+  // register
+  register: (request, response) => {
+    response.json({ message: "recipe created sucessfully" });
+  },
+  // login
+  login: (request, response) => {
+    response.json({ message: "login created sucessfully" });
+  },
+  // logout
+  logout: (request, response) => {
+    response.json({ message: "logout sucessfully done" });
+  },
+  // register
+  me: (request, response) => {
+    response.json({ message: "me created sucessfully" });
+  },
+};
+module.exports = authController;
