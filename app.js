b416c6953dd73897f4b213fd41ddd962d6649497
sugu2344 <suganesh234@gmail.com>
2025-01-04 15:20:10 +0530

    default auth route done

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const logger = require("./utils/logger");
 const morgan = require("morgan");
 const ErrorRoute = require("./utils/error");
+const authRouter = require("./routes/authroutes");
 const app = express();
 // middeware to log
 app.use(logger);
 app.use(morgan("dev"));
+
+// route path
+app.use("/auth",authRouter);
 
 // error route
 app.use(ErrorRoute);
---
+++ routes/authroutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const authController = require("../controller/authcontroller")
+const authRouter = express.Router();
+authRouter.post("/register", authController.register);
+authRouter.post("/login", authController.login);
+authRouter.post("/logout", authController.logout);
+authRouter.get("/me",authController.me);
+module.exports = authRouter;
+
+
+
