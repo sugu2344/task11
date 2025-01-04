f0234a0e4d8dbb870410297740755d3486d6ebcd
sugu2344 <suganesh234@gmail.com>
2025-01-04 16:20:59 +0530

    user data registered sucessful in db

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const morgan = require("morgan");
 const ErrorRoute = require("./utils/error");
 const authRouter = require("./routes/authroutes");
 const app = express();
+// to parse the datas
+app.use(express.json());
+
 // middeware to log
 app.use(logger);
 app.use(morgan("dev"));
 
 // route path
-app.use("/auth",authRouter);
+app.use("/auth", authRouter);
 
 // error route
 app.use(ErrorRoute);
--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -1,17 +1,30 @@
-const auth = require("../models/users");
+const User = require("../models/users");
 const authController = {
   // register
-  register: (request, response) => {
-    response.json({ message: "auth created sucessfully" });
+  register: async (request, response) => {
+    try {
+      const { name, email, password } = request.body;
+      const newUser = new User({
+        name,
+        email,
+        password,
+      });
+      await newUser.save();
+      response.json({ message: "user registered sucessfully" });
+    } catch (error) {
+      response.status(500).json({ message: error.message });
+    }
+
+    // response.json({ message: "auth created sucessfully" });
   },
   // login
   login: (request, response) => {
     response.json({ message: "login created sucessfully" });
   },
   // logout
-//   logout: (request, response) => {
-//     response.json({ message: "logout sucessfully done" });
-//   },
+  //   logout: (request, response) => {
+  //     response.json({ message: "logout sucessfully done" });
+  //   },
   // register
   me: (request, response) => {
     response.json({ message: "me created sucessfully" });
